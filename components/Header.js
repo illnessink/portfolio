8fed5d589bd276e30367c099895f1a330272b92b
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-03 22:48:03 -0400

    wip: adds styling and motion to links

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,65 @@
-import { REACT_LOADABLE_MANIFEST } from 'next/dist/shared/lib/constants';
 import Link  from 'next/link';
 import React from 'react';
 import Logo from './Logo';
+import { useRouter } from 'next/router';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { motion } from 'framer-motion';
+
 
 function Header() {
+    const router = useRouter();
     return (
         <header 
         className='w-full px-32 py-8 font-medium flex items-center justify-between'>
             <nav className="flex-container">
-                        <Link href="/">Home</Link>
-                        <Link href="/about">About</Link>
-                        <Link href="/contact">Contact</Link>
-                        <Link href="/projects">Projects</Link>
+                        <Link href="/" className='mr-4 relative group'>Home
+                            <span className={`h-[1px] inline-block bg-indigo
+                            absolute left-0 -bottom-0.5
+                            group-hover:w-full transition-[width] ease duration-300
+                            ${router.asPath === "/" ? "w-full" : "w-0"}`}>
+                            &nbsp;</span>
+                        </Link>
+                        <Link href="/about" className='mx-4 relative group'>About
+                            <span className={`h-[1px] inline-block bg-indigo
+                            absolute left-0 -bottom-0.5
+                            group-hover:w-full transition-[width] ease duration-300
+                            ${router.asPath === "/about" ? "w-full" : "w-0"}`}>
+                            &nbsp;</span>
+                        </Link>
+                        <Link href="/contact" className='mx-4 relative group'>Contact
+                            <span className={`h-[1px] inline-block bg-indigo
+                            absolute left-0 -bottom-0.5
+                            group-hover:w-full transition-[width] ease duration-300
+                            ${router.asPath === "/contact" ? "w-full" : "w-0"}`}>
+                            &nbsp;</span>
+                        </Link>
+                        <Link href="/projects" className='ml-4 relative group'>Projects
+                            <span className={`h-[1px] inline-block bg-indigo
+                            absolute left-0 -bottom-0.5
+                            group-hover:w-full transition-[width] ease duration-300
+                            ${router.asPath === "/projects" ? "w-full" : "w-0"}`}>
+                            &nbsp;</span>
+                        </Link>
             </nav>
-            <Logo />
-            <nav>
-                <Link href="/" target={"_blank"}>G</Link>
-                <Link href="/" target={"_blank"}>L</Link>
+            <nav className="flex items-center justify-center flex-wrap">
+                <motion.a href="https://github.com/illnessink" target={"_blank"}
+                whileHover={{y:-2}}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3"
+                >
+                    <BsGithub className='text-3xl' />
+                </motion.a>
+                <motion.a href="https://www.linkedin.com/in/vanessafoerster/" target={"_blank"}
+                whileHover={{y:-2}}
+                whileTap={{scale:0.9}}
+                className="w-6 ml-3"
+                >
+                    <BsLinkedin className='text-3xl' />
+                </motion.a>
             </nav>
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo />
+            </div>
         </header>
     );
 }
