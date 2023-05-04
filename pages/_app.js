5e027a3ebf3db72b1a7f0948fecdf39e8584f8f1
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-03 20:22:57 -0400

    wip: adds font and changes layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import '@/styles/globals.css'
+import {Montserrat} from 'next/font/google'
+import Head from 'next/head'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+
+const montserrat = Montserrat({ 
+  subsets: ['latin'],
+  variable: '--font-mont'
+})
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
+    <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </main>
     </>
   )
 }
