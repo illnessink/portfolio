1fb28c21c8b943407018313e5d145b1986cc4791
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-03 22:45:50 -0400

    adds indigo to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,8 @@ module.exports = {
         dark:"#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96",
-        primaryDark: "#58E6D9"
+        primaryDark: "#58E6D9",
+        indigo: "#0B4F6C"
       }
     },
   },
