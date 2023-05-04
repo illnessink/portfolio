28c76d05d4db81b1399e58edefa15be7ead2ecf5
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-03 20:24:40 -0400

    wip: changes layout and styles with tailwind

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,26 @@
+import { REACT_LOADABLE_MANIFEST } from 'next/dist/shared/lib/constants';
 import Link  from 'next/link';
+import React from 'react';
+import Logo from './Logo';
 
 function Header() {
     return (
-        <header>
-            <div className="container">
-            <h1>
-                <Link href="/">Vanessa Foerster</Link>
-            </h1>
+        <header 
+        className='w-full px-32 py-8 font-medium flex items-center justify-between'>
             <nav className="flex-container">
-                <ul>
-                    <li>
                         <Link href="/">Home</Link>
-                    </li>
-                    <li>
                         <Link href="/about">About</Link>
-                    </li>
-                    <li>
                         <Link href="/contact">Contact</Link>
-                    </li>
-                    <li>
                         <Link href="/projects">Projects</Link>
-                    </li>
-                </ul>
             </nav>
-            <style jsx>{`
-                .container {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                    background-color: dodgerblue;
-                    height: 3.125rem;
-                    color: white;
-                }
-                .flex-container {
-                    display: flex;
-                    align-items: center;
-                }
-                ul {
-                    display: flex;
-                    align-items: center;
-                    list-style: none;
-                }
-                li {
-                    margin-left: 1rem;
-                }
-            `}</style>
-            </div>
+            <Logo />
+            <nav>
+                <Link href="/" target={"_blank"}>G</Link>
+                <Link href="/" target={"_blank"}>L</Link>
+            </nav>
         </header>
     );
 }
 
 export default Header;
+
