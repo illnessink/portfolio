0f63e14e9cc0b6948df36322025a16f3479b8101
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-06 12:54:32 -0400

    wip: adds info and styles index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import Header from '@/components/Header'
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FaFileDownload } from 'react-icons/fa'
+
+import profilePic from 'public/images/developer1 copy.png'
 
 
 export default function Home() {
@@ -10,8 +16,29 @@ export default function Home() {
         <title>Home | Vanessa Foerster's Porfolio</title>
         <meta name="description" content="Personal Portfolio Page for Vanessa Foerster" />
       </Head>
-      <main>
-        <h1>Home Page</h1>
+      <main className='flex items-center text-dark w-full min-h-screen'>
+        <Layout className='pt-0'>
+          <div className="flex items-center justify-between w-full">
+            <div className='w-1/2'>
+              <Image src={profilePic} alt="Vanessa Foerster" ClassName="w-full h-auto" />
+            </div>
+            <div className='w-1/2 flex flex-col items-center self-center'>
+              <AnimatedText text="A fusion of code and creativity, bringing your design visions to life" 
+              className='!text-6xl !text-left'/>
+              <p className='my-4 text-base font-medium'>Creative software engineer, driving better user experiences with aesthetics and functionality.
+                Explore my latest projects, showcasing my skills in web development, design, and more.
+              </p>
+              <div className='flex items-center self-start mt-2'>
+                <Link href="/Resume 2023.pdf" target={'_blank'} 
+                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+                hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'
+                download={true}
+                >Resume <FaFileDownload className='ml-2' />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
   )
