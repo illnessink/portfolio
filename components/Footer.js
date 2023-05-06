97aadb2a3ba572cd24a27a227617e2de5a2112e4
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-06 12:53:49 -0400

    styles footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
+import Layout from "./Layout";
+
 function Footer() {
   return (
-    <footer>
-      <p>Copyright &copy; All Rights Reserved {new Date().getFullYear()} Vanessa Foerster</p>
+    <footer className='w-full border-t-2 border-solid border-indigo font-medium text-lg'>
+        <Layout className='py-6 flex items-center justify-center'>
+            <span>Copyright &copy; All Rights Reserved {new Date().getFullYear()} Vanessa Foerster</span>
+        </Layout>
     </footer>
   );
 }
