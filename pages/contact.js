333d06bc8cc730de2bd0ba4470bf0a2e0f79dcd1
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-09 22:02:17 -0400

    changes contact page

--- pages/contact.js
+++ pages/contact.js
@@ -93,53 +93,18 @@ const Contact = () => {
             <title>Vanessa Foerster | Contact Page</title>
             <meta name="descriptin" content="Contact Page for Vanessa Foerster" />
         </Head>
-        <main className='flex w-full flex-col items-center justify-center dark:text-light'>
+        <main className='flex w-full items-center justify-center dark:text-light'>
             <Layout className="pt-16">
                 <AnimatedText text="Let's Connect" className="mb-10"/>
-    <div id="contact" className="w-full lg:h-screen ">
-      <div className="max-w-[1240px] m-auto p-2  w-full">
-        <div className="grid lg:grid-cols-5 gap-8">
-          <div className="col-span-3 lg:col-span-2 w-full h-full shadow shadow-gray-400 rounded-xl p-4 dark:bg-gray-600">
-            <div className="lg:p-4 h-full rounded-xl">
-              <div>
-                <img
-                  className="rounded-xl hover:scale-105 ease-in duration-300"
-                  src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-                  alt="hands typing on laptop"
-                />
-              </div>
-              <div>
-                <h2 className="py-2 font-bold text-xl">Vanessa Foerster</h2>
-                <p>Software Developer</p>
-                <p className="py-4">
-                  I am available for freelance or full-time postions. Let's
-                  talk!
-                </p>
-              </div>
-              <div>
-                <p className="uppercase pt-8">Connect With Me</p>
-                <div className="flex items-center py-4">
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-6">
-                    <a href="https://www.linkedin.com/in/vanessafoerster/"  target="_blank">
-                        <FaLinkedin size={25}  />
-                    </a>
-                  </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <a href="https://github.com/illnessink" target="_blank">
-                        <FaGithub size={25} />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-span-3 w-full h-auto shadow shadow-gray-400 rounded-xl lg:p-4 dark:bg-gray-600">
+    <div id="contact" className="w-full">
+      <div className="max-w-[1240px] m-auto p-2  w-full flex items-center justify-center">
+       
+          <div className="w-1/2 h-auto shadow shadow-gray-400 rounded-xl lg:p-4 dark:bg-gray-600">
             <div className="p-4">
               {ContactForm}
             </div>
           </div>
         </div>
-      </div>
     </div>
     </Layout>
     </main>
