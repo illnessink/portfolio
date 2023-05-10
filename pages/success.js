33f5370ffbfab9d5a87c5eca60346419ed7a2fdd
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-09 21:40:12 -0400

    wip: creates success page for form submit

---
+++ pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const success = () => {
+  return (
+    <div className='flex items-center justify-center w-fill mt-10 mb-10'>
+        <h1 className='text-2xl text-bold dark:text-light'>Success! Form Sent!</h1>
+    </div>
+  )
+}
+
+export default success
