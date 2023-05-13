39b59f4ea49d0d0db8a0415568d610847f3fae3c
Vanessa Foerster <nessfoerster@gmail.com>
2023-05-13 14:24:21 -0400

    adds descriptions and accordian feature

--- pages/projects.js
+++ pages/projects.js
@@ -10,18 +10,30 @@ import downtomatch from '@/public/images/downtomatch.png';
 import happymask from '@/public/images/happymaskshop.png';
 import TransitionEffect from '@/components/TransitionEffect';
 
-const FeaturedProject  = ({title, summary, img, link, github}) => {
+const FeaturedProject  = ({title, technology,  summary, img, link, github}) => {
     return (
-        <article className='w-full flex items-center justify-between rounded-3xl bg-light shadow-2xl 
+        <article className='w-full flex items-center justify-between p-6 rounded-3xl bg-light shadow-2xl 
         dark:bg-gray-600 lg:flex-col lg:p-8 xs:rounded-2xl xs:p-2'>
-            <div className='w-1/2 overflow-hidden rounded-lg p-12 lg:w-full lg:p-4 sm:p-0'>
+            <div className='w-1/2 overflow-hidden rounded-lg lg:w-full lg:p-4 sm:p-0'>
                 <Image src={img} alt={title} className='w-full h-auto border-none rounded-lg' />
             </div>
-            <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full'>
+            <div className='w-1/2 flex flex-col items-start justify-between px-6 lg:w-full'>
                 <Link href={link} target='_blank'>
                     <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-3xl'>{title}</h2>
                 </Link>
-                <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+                <div className="py-2">
+			        <details className="group">
+				        <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
+					        <span className='text-dark dark:text-light'> About {title}</span>
+					        <span className="transition group-open:rotate-180">
+                                <svg className="dark:stroke-white" fill="none" height="24" shapeRendering="geometricPrecision" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+				        </summary>
+				        <p className="my-2 font-medium text-dark dark:text-light mt-3 group-open:animate-fadeIn">{summary}</p>
+			        </details>
+		        </div>
+                <p className='my-2 font-medium text-dark dark:text-light'>{technology}</p>
                 <div className='mt-2 flex items-center'>
                     <Link href={github} target='_blank'
                     className='rounded-lg bg-dark text-light p-2 px=6 text-lg font-semibold
@@ -39,17 +51,29 @@ const FeaturedProject  = ({title, summary, img, link, github}) => {
     )
 }
 
-const Project = ({title, summary, img, link, github}) => {
+const Project = ({title, technology, summary, img, link, github}) => {
     return (
         <article className='w-full rounded-3xl bg-light shadow-2xl p-6 mt-10 dark:bg-gray-600 xs:p-2'>
             <div className='w-full overflow-hidden rounded-lg '>
                 <Image src={img} alt={title} className='w-full h-auto border-none rounded-lg' />
             </div>
-            <div className='w-full flex flex-col items-start justify-between pl-6'>
+            <div className='w-full flex flex-col items-start justify-between px-6'>
                 <Link href={link} target='_blank'>
                     <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-3xl'>{title}</h2>
                 </Link>
-                <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+                <div className="py-2">
+			        <details className="group">
+				        <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
+					        <span className='text-dark dark:text-light'> About {title}</span>
+					        <span className="transition group-open:rotate-180">
+                                <svg className="dark:stroke-white" fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+				        </summary>
+				        <p className="my-2 font-medium text-dark dark:text-light mt-3 group-open:animate-fadeIn">{summary}</p>
+			        </details>
+		        </div>
+                <p className='my-2 font-medium text-dark dark:text-light'>{technology}</p>
                 <div className='mt-2 flex items-center'>
                     <Link href={github} target='_blank'
                     className='rounded-lg bg-dark text-light p-2 px=6 text-lg font-semibold
@@ -83,7 +107,21 @@ function Projects() {
                     <div className='col-span-12'>
                         <FeaturedProject 
                             title='Yarn Cat'
-                            summary='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                            technology="HTML | CSS | JavaScript | Python | Django | PostgreSQL | YouTube API | Bootstrap"
+                            summary="Yarn Cat is a web app I created using the full-stack Django framework. It allows users to 
+                            easily upload and manage their crafting projects. With Yarn Cat, users can add project descriptions, 
+                            upload photos, and track time spent on each project.
+
+                            Authentication is handled through Django's built-in system, while authorization is implemented to 
+                            restrict access to specific resources. The app is powered by PostgreSQL, ensuring that data is safely 
+                            and reliably stored.
+                            
+                            Users can search and favorite tutorial videos using the YouTube API to help them learn new techniques 
+                            and enhance their crafting skills. Yarn Cat is deployed online using Heroku and features professional 
+                            styling with CSS and Bootstrap.
+                            
+                            As the sole creator of Yarn Cat, I'm proud to offer a user-friendly and visually appealing app that 
+                            helps crafters stay organized and productive."
                             img={yarncatImg}
                             link='https://yarncat.herokuapp.com/'
                             github='https://github.com/illnessink/yarn_cat'
@@ -92,7 +130,15 @@ function Projects() {
                     <div className='col-span-6 mr-2 sm:col-span-12'>
                         <Project 
                         title='HangMan'
-                        summary='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                        technology="HTML | CSS | JavaScript | JQuery | Google Fonts | Wordnik API"
+                        summary="I created a  simple Hangman website that uses HTML, CSS, JavaScript, 
+                        AJAX and the Wordnik API to bring the classic game to life. Players of all ages 
+                        can enjoy it because it is designed to be immersive and challenging. The Wordnik 
+                        API provides a random word for each game, and players can guess letters to discover 
+                        the hidden word. AJAX is used to seamlessly communicate with the API to make sure 
+                        that words are always new. The website offers an enjoyable Hangman experience that 
+                        challenges players' wordplay abilities, expands their vocabulary, and provides hours 
+                        of entertainment."
                         img={hangman}
                         link='https://illnessink.github.io/proj1-hangman-game/'
                         github='https://github.com/illnessink/proj1-hangman-game'
@@ -101,7 +147,16 @@ function Projects() {
                     <div className='col-span-6 sm:col-span-12'>
                     <Project 
                         title='Down to Match'
-                        summary='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                        technology="React | Node.js | Express | MongoDB | Socket.IO | Firebase | Socket.IO | JavaScript | Bootstrap"
+                        summary="Down to Match (DTM) is a dating application designed to help users find compatible matches 
+                        based on their interests and preferences. Users can create a profile with essential personal 
+                        details such as name, last name, location, interests, and hobbies. Once their profile is complete, 
+                        they can take a 10-question compatibility quiz to determine their compatibility percentage with 
+                        other users on the platform. Upon receiving compatibility results, users can view their matches, 
+                        and explore their profiles before engaging in real-time chats utilizing Socket.IO. The 
+                        MERN-stack technology used in the application ensures seamless interaction between all components 
+                        while maintaining privacy and data security. With its interactive and well-styled frontend, users 
+                        can enjoy seamless communication with the backend for an engaging and user-friendly experience."
                         img={downtomatch}
                         link='https://downtomatch.netlify.app/'
                         github='https://github.com/illnessink/dtm-frontend'
@@ -110,7 +165,15 @@ function Projects() {
                     <div className='col-span-12 mt-10'>
                         <FeaturedProject 
                             title='Happy Mask Shop'
-                            summary='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                            technology="HTML | CSS | JavaScript | Express | Google Fonts | MongoDB | Node.js"
+                            summary="The Happy Mask Shop is a fully-featured web app built with Node, Express, 
+                            and MongoDB, and it's inspired by the memorable Ocarina of Time and Majora's Mask 
+                            from the much-loved Zelda game series. This tribute to the in-game shop and mask-collecting 
+                            adventure has a &quot;masks&quot; model that comes with complete CRUD operations, 
+                            letting users easily interact with the data entity. On the site, you can &quot;buy&quot; 
+                            and &quot;sell&quot; masks, add new masks to your collection, delete them, and see 
+                            all the masks you own. Immerse yourself in the nostalgic world of Zelda while showcasing 
+                            the power of a full-stack application."
                             img={happymask}
                             link='https://happy-mask-shop.herokuapp.com/masks'
                             github='https://github.com/illnessink/Happy-Mask-Shop'
